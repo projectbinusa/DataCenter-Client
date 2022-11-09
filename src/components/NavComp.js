ee95ad1f87f5eab332008a6b5d72f688446ec9ba
syarifahaha <laylarobiatussyarifah.tkj.2021@binusasmg.sch.id>
2022-11-09 13:33:51 +0700

    update

--- src/components/NavComp.js
+++ src/components/NavComp.js
@@ -39,7 +39,7 @@ export default function NavComp() {
                     className="text-gray-500 transition hover:text-gray-500/75"
                     href="/table"
                   >
-                  Table
+                    Table
                   </a>
                 </li>
               </ul>
@@ -72,24 +72,6 @@ export default function NavComp() {
                   </a>
                 )}
               </div>
-
-              <button className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden">
-                <span className="sr-only">Toggle menu</span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              </button>
             </div>
           </div>
         </div>
