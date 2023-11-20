0251846980a49ed1b7da60354b323c024b900d38
DavinaCahyani <davinacahyaniputri@gmail.com>
2023-11-20 15:01:08 +0700

    update export dan import guru

--- src/pages/guru/TableGuru.js
+++ src/pages/guru/TableGuru.js
@@ -15,6 +15,7 @@ import { faUsers, faUserFriends } from "@fortawesome/free-solid-svg-icons";
 export default function TableGuru() {
   const [guru, setGuru] = useState([]);
   const [isChecked, setIsChecked] = useState([]);
+  const [modal, setModal] = useState(false);
 
   const [state, setState] = useState({
     options: {
@@ -373,6 +374,69 @@ export default function TableGuru() {
     }
   };
 
+  const download = async () => {
+    await Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, download it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios({
+          url:
+            "http://localhost:8080/api/excel-guru/download-guru/" +
+            localStorage.getItem("sekolahId"),
+          method: "GET",
+          responseType: "blob",
+        }).then((response) => {
+          var fileURL = window.URL.createObjectURL(new Blob([response.data]));
+          var fileLink = document.createElement("a");
+
+          fileLink.href = fileURL;
+          fileLink.setAttribute("download", "data-guru.xlsx");
+          document.body.appendChild(fileLink);
+
+          fileLink.click();
+        });
+        Swal.fire("Download!", "Your file has been download.", "success");
+      }
+    });
+  };
+
+  const downloadFormat = async () => {
+    await Swal.fire({
+      title: "Yakin ingin mendownload?",
+      text: "Ini adalah file format excel untuk mengimport data guru",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#0b409c",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Ya, download!",
+      cancelButtonText: "Batal",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios({
+          url: "http://localhost:8080/api/excel-guru/download-guru/",
+          method: "GET",
+          responseType: "blob",
+        }).then((response) => {
+          var fileURL = window.URL.createObjectURL(new Blob([response.data]));
+          var fileLink = document.createElement("a");
+
+          fileLink.href = fileURL;
+          fileLink.setAttribute("download", "format-excel-guru.xlsx");
+          document.body.appendChild(fileLink);
+
+          fileLink.click();
+        });
+        Swal.fire("Download!", "File berhasil di download.", "success");
+      }
+    });
+  };
+
   useEffect(() => {
     getAll();
     data();
@@ -566,9 +630,32 @@ export default function TableGuru() {
             </div>
           </div>
           <br></br>
+
           {/* tabel guru start */}
 
           <div className="p-5 bg-white">
+            <div className="grid justify-center">
+              <div className="grid grid-cols-1 md:flex gap-3 mt-6">
+                {guru.length === 0 ? (
+                  <></>
+                ) : (
+                  <button
+                    className="text-white w-56 add-siswa active:bg-slate-300 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                    onClick={download}
+                  >
+                    Download Data
+                  </button>
+                )}
+                <button
+                  className="text-white w-56 add-siswa active:bg-slate-300 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                  type="button"
+                  onClick={() => setModal(true)}
+                >
+                  Import Data
+                </button>
+              </div>
+            </div>
             <div className="overflow-hidden overflow-x-auto rounded-lg border border-gray-200 p-5">
               <table
                 className="min-w-full divide-gray-200 text-center p-5"
@@ -721,6 +808,87 @@ export default function TableGuru() {
             </div>
           </div>
           {/* tabel siswa end */}
+          {/* modal import dan download format data start */}
+          {modal ? (
+            <>
+              <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                  {/*content*/}
+                  <div className="border-1 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                    {/*header*/}
+                    <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                      <h3 className="text-3xl font-semibold">Import Data</h3>
+                      <button
+                        className="p-1 ml-auto bg-transparent border-0 opacity-20 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                        onClick={() => setModal(false)}
+                      >
+                        <span className="bg-transparent text-black h-6 w-6 text-2xl block outline-none focus:outline-none">
+                          ×
+                        </span>
+                      </button>
+                    </div>
+                    {/*body*/}
+                    <div className="relative flex-auto">
+                      <form
+                        action=""
+                        className="space-y-4 p-3"
+                        // onSubmit={importExcel}
+                      >
+                        <div>
+                          <p className="m-5 text-lg font-medium">
+                            download file dibawah untuk menginput data siswa
+                            anda. (format sudah tertulis)
+                          </p>
+                          <button
+                            className="text-white add-siswa active:bg-slate-300 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none ease-linear transition-all duration-150"
+                            type="button"
+                            onClick={downloadFormat}
+                          >
+                            Download File
+                          </button>
+                        </div>
+                        <div className="py-3">
+                          <p className="text-lg font-medium mt-5">
+                            nb: file excel tidak boleh ada kolom yang blank
+                          </p>
+                          <p className="mb-5 text-lg font-medium">
+                            jika sudah menginputkan data siswa ke dalam file
+                            yang sudah anda download tadi, selanjutnya bisa anda
+                            importkan dengan menekan tombol dibawah:
+                          </p>
+                          <input
+                            autoComplete="off"
+                            type="file"
+                            accept=".xlsx"
+                            // onChange={(e) => setExcel(e.target.files[0])}
+                            className="border-2 rounded-md p-3"
+                          />
+                        </div>
+                        <div className="flex items-center justify-end gap-5 p-3 border-t border-solid border-slate-200 rounded-b">
+                          <button
+                            className="text-white bg-red-700 font-bold uppercase px-6 py-3.5 rounded-md text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                            type="button"
+                            onClick={() => setModal(false)}
+                          >
+                            Batal
+                          </button>
+                          <button
+                            className="bg-gradient-to-r from-[#0b409c] to-[#10316b] text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                            type="submit"
+                          >
+                            Import
+                          </button>
+                        </div>
+                      </form>
+                    </div>
+                    {/*footer*/}
+                  </div>
+                </div>
+              </div>
+              <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+            </>
+          ) : null}
+          {/* modal import dan download format data end */}
         </div>
       </div>
     </>
