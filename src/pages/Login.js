e9337dc7fed5e5ebd913ecf835e3f95516e4bb3c
ZiyanHaidar <ziyanhaidar809@gmail.com>
2023-12-06 15:33:17 +0700

    memperbaiki login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -64,8 +64,7 @@ export default function Login() {
     setPasswordType("password");
   };
 
-  return (
-    <div>
+    return (
       <div className="sec-log">
         <div className="mx-auto max-w-screen-xl px-4 py-1 sm:px-6 lg:px-8">
           <div className="mx-auto max-w-lg">
@@ -83,11 +82,10 @@ export default function Login() {
                 onSubmit={login}
               >
                 <div className="pb-3">
-                  <p className="text-2xl font-medium pb-2">
-                    Masuk ke akun Anda
-                  </p>
-                  <p className="text-sm">masukkan email dan kata sandi Anda</p>
+                  <p className="text-3xl font-bold pb-2 text-center">Selamat Datang!</p>
+                  <p className="text-lg text-center" style={{ color: '#718096' }}>Masukkan email dan kata sandi Anda</p>
                 </div>
+  
 
                 <div className="p-1">
                   <div className="relative mt-1 text-black">
@@ -103,35 +101,22 @@ export default function Login() {
                   </div>
                 </div>
 
-                <div className="p-1">
-                  <div className="relative  mt-1 text-black">
+                <div className="p-1 relative">
+                  <div className="relative mt-1 text-black">
                     <input
                       autoComplete="off"
                       type={passwordType}
-                      className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
+                      className="w-full rounded-lg border-gray-200 py-4 px-3 text-sm shadow-sm pr-12" // Added 'pr-12' for padding-right to accommodate the eye icon
                       placeholder="Kata Sandi"
                       value={password}
                       onChange={(e) => setPassword(e.target.value)}
                     />
-
                     <span
                       onClick={togglePassword}
-                      className="absolute inset-y-0 right-4 inline-flex items-center"
+                      className="absolute inset-y-0 right-3 flex items-center cursor-pointer"
                     >
                       {passwordType === "password" ? (
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="16"
-                          height="16"
-                          fill="currentColor"
-                          className="bi bi-eye"
-                          viewBox="0 0 16 16"
-                        >
-                          <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
-                          <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
-                        </svg>
-                      ) : (
-                        <svg
+                       <svg
                           xmlns="http://www.w3.org/2000/svg"
                           width="16"
                           height="16"
@@ -139,10 +124,23 @@ export default function Login() {
                           className="bi bi-eye-slash"
                           viewBox="0 0 16 16"
                         >
-                          <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z" />
+                       
                           <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z" />
                           <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z" />
                         </svg>
+                      ) : (
+                        <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-eye"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                      </svg>
+                        
                       )}
                     </span>
                   </div>
@@ -168,6 +166,5 @@ export default function Login() {
           </div>
         </div>
       </div>
-    </div>
   );
 }
